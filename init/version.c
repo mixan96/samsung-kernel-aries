d99c6b44233f0a3b01357ca1bc1012ffbd6f13bd
kasperhettinga <kasper.hettinga@gmail.com>
2013-12-07 15:56:54 +0100

    Bump kernel version to 0.603_twrp_swappedSD
    Change-Id: I78c417fab520f6f5f1fdc564242a53411f57aeda

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (Mackay_kernel_0.603_cwm_swappedSD)"
+	" (Mackay_kernel_0.603_twrp_swappedSD)"
 	" (" LINUX_COMPILER ") %s\n";

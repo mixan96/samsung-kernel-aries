3bf8c245a19b6329a6f36c5f8d320fe889d9a4de
kasperhettinga <kasper.hettinga@gmail.com>
2014-02-13 17:42:48 +0100

    Move up to 0.700 Nameless as version number

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (Mackay_kernel_0.607_twrp)"
+	" (Mackay_kernel_0.700_nameless)"
 	" (" LINUX_COMPILER ") %s\n";
